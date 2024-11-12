fb156ad7c603b77c5cb1cf973d02c89a6b1735cf
Idealist_CBT <417304028@qq.com>
2024-11-13 01:49:23 +0800

    备份

--- script_controller.py
+++ script_controller.py
@@ -9,225 +9,210 @@ import ctypes
 import inspect
 from utils import setup_logger
 
-module_names = {
-    "scripts.yjk": "元件库用例",
-    "scripts.lxbj": "离线编辑用例",
-    "scripts.jbgn": "基本功能用例",
-    "scripts.kjj": "快捷键用例",
-    "scripts.spc": "SPC用例"
-}
+class HomeScreen(tk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.master = master
+        self.pack()
+        self.create_widgets()
 
-def _async_raise(tid, exctype):
-    """Raises an exception in the threads with id tid"""
-    tid = ctypes.c_long(tid)
-    if not inspect.isclass(exctype):
-        exctype = type(exctype)
-    res = ctypes.pythonapi.PyThreadState_SetAsyncExc(tid, ctypes.py_object(exctype))
-    if res == 0:
-        raise ValueError("invalid thread id")
-    elif res != 1:
-        ctypes.pythonapi.PyThreadState_SetAsyncExc(tid, None)
-        raise SystemError("PyThreadState_SetAsyncExc failed")
+    def create_widgets(self):
+        self.selected_icon = tk.StringVar(value="AOI")
 
-def stop_thread(thread):
-    _async_raise(thread.ident, SystemExit)
+        style = ttk.Style()
+        style.configure("TButton", font=("Segoe UI", 12), padding=10)
+        style.configure("Selected.TButton", background="darkblue", foreground="white", borderwidth=2, relief="solid")
+        style.configure("Unselected.TButton", background="lightgray", foreground="black", borderwidth=2, relief="ridge")
 
-def update_method_status(method_name):
-    status = test_case_status.get(method_name, "unknown")
-    for i in range(method_listbox.size()):
-        if method_listbox.get(i) == method_name:
-            method_listbox.selection_clear(i)  # 取消选中
-            if status == "success":
-                method_listbox.itemconfig(i, {'bg':'green'})
-            elif status == "failed":
-                method_listbox.itemconfig(i, {'bg':'red'})
+        self.label = tk.Label(self, text="自动化测试", font=("Segoe UI", 18))
+        self.label.pack(pady=20)
+
+        self.icon_frame = tk.Frame(self)
+        self.icon_frame.pack(pady=20)
+
+        self.aoi_icon = ttk.Button(
+            self.icon_frame, text="AOI", command=self.toggle_aoi, style="Selected.TButton"
+        )
+        self.aoi_icon.pack(padx=10, pady=10)
+
+        self.button_frame = tk.Frame(self)
+        self.button_frame.pack(pady=20)
+
+        self.confirm_button = ttk.Button(self.button_frame, text="确认", command=self.open_aoi_detail)
+        self.confirm_button.pack(side="left", padx=10)
+
+        self.exit_button = ttk.Button(self.button_frame, text="退出", command=self.master.quit)
+        self.exit_button.pack(side="right", padx=10)
+
+    def toggle_aoi(self):
+        if self.selected_icon.get() == "AOI":
+            self.selected_icon.set("")
+            self.aoi_icon.configure(style="Unselected.TButton")
+        else:
+            self.selected_icon.set("AOI")
+            self.aoi_icon.configure(style="Selected.TButton")
+
+    def open_aoi_detail(self):
+        if self.selected_icon.get() == "AOI":
+            self.master.switch_frame(AOIDetailScreen)
+
+class AOIDetailScreen(tk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.master = master
+        self.pack()
+        self.create_widgets()
+
+    def create_widgets(self):
+        style = ttk.Style()
+        style.configure("TButton", font=("Segoe UI", 10), padding=5, background="#f0f0f0", foreground="#333", borderwidth=1, relief="solid")
+        style.map("TButton",
+                  background=[("active", "#e0e0e0"), ("disabled", "#f0f0f0")],
+                  foreground=[("active", "#000"), ("disabled", "#999")])
+
+        self.system_label = ttk.Label(self, text="系统:")
+        self.system_label.grid(row=0, column=0, padx=5, pady=5, sticky="e")
+
+        self.system_combobox = ttk.Combobox(self, values=["UI", "中间层", "MES", "SPC", "RV"], width=10)
+        self.system_combobox.current(0)
+        self.system_combobox.grid(row=0, column=1, padx=5, pady=5)
+
+        self.case_label = ttk.Label(self, text="用例:")
+        self.case_label.grid(row=0, column=2, padx=5, pady=5, sticky="e")
+
+        self.modules = {
+            "离线编辑": lxbj,
+            "元件库": yjk,
+            "基本功能": jbgn,
+            "快捷键": kjj,
+            "SPC": spc
+        }
+
+        self.case_combobox = ttk.Combobox(self, values=list(self.modules.keys()), width=10)
+        self.case_combobox.bind("<<ComboboxSelected>>", self.update_table)
+        self.case_combobox.grid(row=0, column=3, padx=5, pady=5)
+
+        self.search_frame = tk.Frame(self)
+        self.search_frame.grid(row=1, column=0, columnspan=4, pady=10, padx=20, sticky="w")
+
+        self.search_entry = ttk.Entry(self.search_frame, width=20, foreground="gray")
+        self.search_entry.insert(0, "输入用例编码搜索")
+        self.search_entry.bind("<FocusIn>", self.clear_placeholder)
+        self.search_entry.bind("<FocusOut>", self.add_placeholder)
+        self.search_entry.pack(side="left", fill="x", expand=True)
+
+        self.search_button = ttk.Button(self.search_frame, text="搜索", style="TButton", width=5, command=self.search_methods)
+        self.search_button.pack(side="left", fill="y", padx=0)
+
+        self.start_button = ttk.Button(self.search_frame, text="开始执行", style="TButton", width=10)
+        self.start_button.pack(side="left", padx=5)
+
+        self.stop_button = ttk.Button(self.search_frame, text="停止执行", style="TButton", width=10)
+        self.stop_button.pack(side="left", padx=5)
+
+        self.table = ttk.Treeview(self, columns=("编码", "状态", "操作"), show="headings", height=10)
+        self.table.heading("编码", text="编码")
+        self.table.heading("状态", text="状态")
+        self.table.heading("操作", text="操作")
+        self.table.grid(row=2, column=0, columnspan=6, padx=5, pady=5, sticky="nsew")
+
+        self.table.column("编码", anchor="center", width=100)
+        self.table.column("状态", anchor="center", width=100)
+        self.table.column("操作", anchor="center", width=200)
+
+        self.more_label = tk.Label(self, text="点击查询更多...", fg="blue", cursor="hand2")
+        self.more_label.grid(row=3, column=0, columnspan=6, pady=10)
+        self.more_label.bind("<Button-1>", self.more_info)
+
+    def update_table(self, event=None):
+        module_name = self.case_combobox.get()
+        module = self.modules.get(module_name)
+        if module:
+            methods = inspect.getmembers(module, inspect.isfunction)
+            self.table.delete(*self.table.get_children())
+            for method_name, _ in methods:
+                self.add_table_row(method_name)
+
+    def add_table_row(self, method_name):
+        item = self.table.insert("", "end", values=(method_name, "未执行"))
+        self.add_buttons_to_row(item, method_name)
+
+    def add_buttons_to_row(self, item, method_name):
+        # 获取Treeview的bbox来确定按钮位置
+        bbox = self.table.bbox(item, column="操作")
+        if not bbox:
+            return
+
+        # 创建一个Canvas来放置按钮
+        canvas = tk.Canvas(self.table, width=bbox[2], height=bbox[3])
+        canvas.place(x=bbox[0], y=bbox[1])
+
+        # 创建按钮
+        execute_button = tk.Button(canvas, text="执行这条", command=lambda: self.execute_method(method_name))
+        execute_down_button = tk.Button(canvas, text="这条往下", command=lambda: self.execute_down(method_name))
+
+        # 布局按钮
+        execute_button.pack(side="left", padx=5)
+        execute_down_button.pack(side="left", padx=5)
+
+    def execute_method(self, method_name):
+        print(f"执行方法: {method_name}")
+
+    def execute_down(self, method_name):
+        print(f"执行从方法: {method_name} 开始的所有方法")
+
+    def search_methods(self):
+        search_text = self.search_entry.get().strip()
+        for item in self.table.get_children():
+            method_name = self.table.item(item, "values")[0]
+            if search_text.lower() in method_name.lower() or not search_text:
+                self.table.reattach(item, '', 'end')
             else:
-                method_listbox.itemconfig(i, {'bg':'white'})
+                self.table.detach(item)
 
-def run_all():
-    selected_module_key = next(key for key, value in module_names.items() if value == module_var.get())
-    module = importlib.import_module(selected_module_key)
-    methods = [getattr(module, func) for func in dir(module) if callable(getattr(module, func)) and func.startswith(selected_module_key.split('.')[-1])]
-    
-    def run_method(index):
-        if index < len(methods) and running_event.is_set():
-            method = methods[index]
-            current_method_var.set(method.__name__)
-            root.after(3000, lambda: execute_method(method, index))
+    def clear_placeholder(self, event):
+        if self.search_entry.get() == "输入用例编码搜索":
+            self.search_entry.delete(0, tk.END)
+            self.search_entry.configure(foreground="black")
+
+    def add_placeholder(self, event):
+        if not self.search_entry.get():
+            self.search_entry.insert(0, "输入用例编码搜索")
+            self.search_entry.configure(foreground="gray")
+
+    def more_info(self, event):
+        # TODO: Add functionality for more info
+        pass
+
+class Application(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title("自动化测试")
+        self.geometry("600x600")  # 调整窗口大小
+        self.center_window()
+        self.current_frame = None
+        self.switch_frame(HomeScreen)  # 先进入HomeScreen
+
+    def center_window(self):
+        self.update_idletasks()
+        width = self.winfo_width()
+        height = self.winfo_height()
+        x = (self.winfo_screenwidth() // 2) - (width // 2)
+        y = (self.winfo_screenheight() // 2) - (height // 2)
+        self.geometry(f"{width}x{height}+{x}+{y}")
+
+    def switch_frame(self, frame_class):
+        new_frame = frame_class(self)
+        if self.current_frame is not None:
+            self.current_frame.destroy()
+        self.current_frame = new_frame
+        self.current_frame.pack()
+        if isinstance(new_frame, AOIDetailScreen):
+            self.geometry("600x600")  # Detail界面大小
         else:
-            root.after(0, update_status, "所有方法执行完毕")
-            root.after(10000, lambda: update_status(""))
-            current_method_var.set("")
-    
-    def execute_method(method, index):
-        def run_in_thread():
-            try:
-                result, error = method()
-                if running_event.is_set():
-                    root.after(0, update_status, f"{method.__name__} 方法执行完毕")
-                    root.after(10000, lambda: update_status(""))
-                    root.after(0, update_method_status, method.__name__)
-                    root.after(3000, lambda: run_method(index + 1))
-            except SystemExit:
-                logger.info(f"{method.__name__} 方法被终止")
-            except Exception as e:
-                logger.error(f"{method.__name__} 方法执行出错: {e}")
-        global current_thread
-        current_thread = Thread(target=run_in_thread)
-        current_thread.start()
+            self.geometry("400x400")  # Home界面大小
 
-    run_method(0)
-
-def run_selected(method_names):
-    selected_module_key = next(key for key, value in module_names.items() if value == module_var.get())
-    module = importlib.import_module(selected_module_key)
-    
-    def run_method(index):
-        if index < len(method_names) and running_event.is_set():
-            method_name = method_names[index]
-            method = getattr(module, method_name)
-            current_method_var.set(method.__name__)
-            root.after(3000, lambda: execute_method(method, index))
-        else:
-            root.after(0, update_status, "选中的方法执行完毕")
-            root.after(5000, lambda: update_status(""))
-    
-    def execute_method(method, index):
-        def run_in_thread():
-            try:
-                result, error = method()
-                if running_event.is_set():
-                    root.after(0, update_status, f"{method.__name__} 方法执行完毕")
-                    root.after(10000, lambda: update_status(""))
-                    root.after(0, update_method_status, method.__name__)
-                    root.after(3000, lambda: run_method(index + 1))
-            except SystemExit:
-                logger.info(f"{method.__name__} 方法被终止")
-            except Exception as e:
-                logger.error(f"{method.__name__} 方法执行出错: {e}")
-        global current_thread
-        current_thread = Thread(target=run_in_thread)
-        current_thread.start()
-
-    run_method(0)
-
-def start_all():
-    global thread
-    try:
-        if thread and thread.is_alive():
-            logger.warning("已有一个用例脚本在运行，请等待其结束后再启动新的用例脚本")
-            return
-    except NameError:
-        thread = None
-    running_event.set()
-    test_case_status.clear()  # 清理状态
-    root.iconify()
-    thread = Thread(target=run_all)
-    thread.start()
-
-def start_selected():
-    global thread
-    try:
-        if thread and thread.is_alive():
-            logger.warning("已有一个用例脚本在运行，请等待其结束后再启动新的用例脚本")
-            return
-    except NameError:
-        thread = None
-    running_event.set()
-    test_case_status.clear()  # 清理状态
-    root.iconify()
-    selected_indices = method_listbox.curselection()
-    selected_methods = [method_listbox.get(i) for i in selected_indices]
-    thread = Thread(target=lambda: run_selected(selected_methods))
-    thread.start()
-
-def terminate_execution():
-    global thread, current_thread
-    running_event.clear()
-    if current_thread.is_alive():
-        logger.info("正在终止当前运行的用例函数")
-        stop_thread(current_thread)  # 强制终止当前线程
-    if thread.is_alive():
-        thread.join(timeout=5)  # 等待线程结束，设置超时时间为5秒
-        if thread.is_alive():
-            logger.error("线程未能在预期时间内结束")
-            stop_thread(thread)  # 强制终止线程
-    update_status("脚本已终止")
-
-def update_status(message):
-    status_var.set(message)
-    root.update()
-
-# 创建主窗口
-root = tk.Tk()
-root.title("脚本控制器")
-root.geometry("510x555")  # 调整窗口大小以确保完全显示
-
-# 配置样式
-style = ttk.Style()
-style.theme_use('clam')
-style.configure('.', font=('Helvetica', 10))
-style.configure('TButton', font=('Helvetica', 10), padding=10, relief='raised', borderwidth=2)
-style.configure('TLabel', font=('Helvetica', 10), padding=6)
-style.configure('TEntry', font=('Helvetica', 10), padding=6)
-
-# 状态和方法执行信息显示区域
-info_frame = ttk.Frame(root)
-info_frame.grid(row=0, column=0, columnspan=2, padx=10, pady=10, sticky="ew")
-
-status_var = tk.StringVar()
-status_label = ttk.Label(info_frame, textvariable=status_var, style='TLabel')
-status_label.grid(row=0, column=0, columnspan=2, sticky="ew")
-
-current_method_var = tk.StringVar()
-current_method_label = ttk.Label(info_frame, text="目前执行：")
-current_method_label.grid(row=1, column=0, sticky="w")
-current_method_entry = ttk.Entry(info_frame, textvariable=current_method_var, state='readonly')
-current_method_entry.grid(row=1, column=1, sticky="ew")
-
-# 下拉框选择模块
-module_var = tk.StringVar(root)
-default_module = "scripts.yjk"
-module_var.set(module_names[default_module])
-
-module_dropdown = ttk.OptionMenu(root, module_var, module_names[default_module], *module_names.values())
-module_dropdown.grid(row=2, column=0, columnspan=2, padx=10, pady=10, sticky="ew")
-
-# 方法列表区域
-list_frame = ttk.Frame(root)
-list_frame.grid(row=3, column=0, padx=10, pady=10, sticky="nsw")
-
-method_listbox = tk.Listbox(list_frame, height=20, selectmode='multiple', width=50)  # 调整列表框大小
-method_listbox.grid(row=0, column=0, sticky="nsw")
-
-# 按钮区域
-button_frame = ttk.Frame(root)
-button_frame.grid(row=3, column=1, padx=10, pady=10, sticky="nse")
-
-run_all_button = ttk.Button(button_frame, text="执行全部", command=start_all)
-run_all_button.grid(row=0, column=0, padx=5, pady=5)
-
-run_selected_button = ttk.Button(button_frame, text="执行选中", command=start_selected)
-run_selected_button.grid(row=1, column=0, padx=5, pady=5)
-
-stop_button = ttk.Button(button_frame, text="终止执行", command=terminate_execution)
-stop_button.grid(row=2, column=0, padx=5, pady=5)
-
-def update_methods():
-    method_listbox.delete(0, tk.END)
-    friendly_name = module_var.get()
-    selected_module = next(key for key, value in module_names.items() if value == friendly_name)
-    module = importlib.import_module(selected_module)
-    method_names = [getattr(module, func) for func in dir(module) if callable(getattr(module, func)) and func.startswith(selected_module.split('.')[-1])]
-    for method_name in method_names:
-        method_listbox.insert(tk.END, method_name.__name__)
-
-# 当下拉框选择改变时更新方法列表
-module_var.trace('w', lambda *args: update_methods())
-
-# 初始化方法列表
-update_methods()
-
-setup_logger()
-
-root.mainloop()
+if __name__ == "__main__":
+    setup_logger()
+    app = Application()
+    app.mainloop()
